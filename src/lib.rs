155ec814e1ba5de970e769ac0119d92c2b5fb474
Heinz N. Gies <heinz@licenser.net>
2019-08-29 18:17:32 +0200

    Add assert_instr file

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-
-#[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
-pub mod arm;
 #[cfg(target_arch = "aarch64")]
 pub mod aarch64;
+#[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
+pub mod arm;
+
+#[cfg(test)]
+mod assert_instr;
 
 #[cfg(test)]
 mod tests {
